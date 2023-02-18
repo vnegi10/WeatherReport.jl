c362ebc0443cdafb1df961db4bf5abe4f37ee98a
Vikas Negi <vikas.negi10@gmail.com>
2023-02-18 18:03:34 +0100

    Working example

--- src/constants.jl
+++ src/constants.jl
@@ -1,2 +1,3 @@
 const URL = "https://api.open-meteo.com/v1/forecast"
-const DF_CITIES = get_cities_lat_long("../data/cities15000.txt")
+const DF_CITIES = joinpath(@__DIR__, "..", "data", 
+                           "cities15000.txt") |> get_cities_lat_long
