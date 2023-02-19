3fbc2d1c7ac84fd2bc27f282f81309c74ce850d7
Vikas Negi <vikas.negi10@gmail.com>
2023-02-19 02:27:06 +0100

    Add tests for temp. forecast

---
+++ test/runtests.jl
@@ -0,0 +1,43 @@
+using Test, Weather
+
+errors = false
+all_tests = false
+
+# Run all tests when no arguments or "all" is specified
+if isempty(ARGS) || "all" in ARGS
+    all_tests = true
+end
+
+# Execute test in a try-catch block for each file
+function execute_test(file::String, ARGS; all_tests::Bool, errors::Bool)
+
+    if all_tests || file in ARGS
+        try
+            include(file)
+            println("\t\033[1m\033[32mPASSED\033[0m: $(file)")
+        catch e
+            println("\t\033[1m\033[31mFAILED\033[0m: $(file)")
+            errors = true
+            showerror(stdout, e, backtrace())
+            println()
+        end
+    end
+
+    if errors
+        @warn "Some tests have failed! Check the results summary above."
+    end
+end
+
+################# All test groups #################
+
+test_files = ["test_temperature.jl"]
+
+###################################################
+
+################# Execute tests ###################
+
+@time for file in test_files
+    execute_test(file, ARGS, all_tests = all_tests, errors = errors)
+end
+
+###################################################
---
+++ test/test_temperature.jl
@@ -0,0 +1,31 @@
+@testset "Check if temperature forecast is available" begin
+
+    for city in ["Eindhoven", "Veldhoven", "Groningen"]
+
+        results = Weather.get_temp_forecast(city)
+        df_temp = results[1]
+
+        rows, cols = size(df_temp)
+
+        @test rows == 24*7
+        @test cols == 2
+
+        @test results[2].timezone == "Europe/Amsterdam"
+
+    end
+
+    for city in ["Haldwani", "Delhi", "Mumbai"]
+
+        results = Weather.get_temp_forecast(city)
+        df_temp = results[1]
+
+        rows, cols = size(df_temp)
+
+        @test rows == 24*7
+        @test cols == 2
+
+        @test results[2].timezone == "Asia/Kolkata"
+
+    end
+
+end
