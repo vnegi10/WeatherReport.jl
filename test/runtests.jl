255051ada8c4c949b6095d93b91038bef228f618
Vikas Negi <vikas.negi10@gmail.com>
2023-02-22 22:08:05 +0100

    Update test logic

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ if isempty(ARGS) || "all" in ARGS
 end
 
 # Execute test in a try-catch block for each file
-function execute_test(file::String, ARGS; all_tests::Bool, errors::Bool)
+function execute_test(file::String, ARGS; all_tests::Bool)
 
     if all_tests || file in ARGS
         try
@@ -17,15 +17,12 @@ function execute_test(file::String, ARGS; all_tests::Bool, errors::Bool)
             println("\t\033[1m\033[32mPASSED\033[0m: $(file)")
         catch e
             println("\t\033[1m\033[31mFAILED\033[0m: $(file)")
-            errors = true
+            global errors = true
             showerror(stdout, e, backtrace())
             println()
         end
     end
 
-    if errors
-        @warn "Some tests have failed! Check the results summary above."
-    end
 end
 
 ################# All test groups #################
@@ -41,7 +38,11 @@ test_files = ["test_hourly.jl",
 ################# Execute tests ###################
 
 @time for file in test_files
-    execute_test(file, ARGS, all_tests = all_tests, errors = errors)
+    execute_test(file, ARGS, all_tests = all_tests)
+end
+
+if errors
+    throw("Some tests have failed! Check the results summary above.")
 end
 
 ###################################################
