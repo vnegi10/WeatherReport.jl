fe7e43cf502e0ad1bd517587550d45725a5225e5
Vikas Negi <vikas.negi10@gmail.com>
2023-02-23 22:34:47 +0100

    Fix spacing issue

--- src/current.jl
+++ src/current.jl
@@ -1,14 +1,14 @@
 function get_current(city::String, i_row::Int64)
 
-	location = fetch_lat_long(city, i_row)
-	lat      = location.latitude
-	long     = location.longitude
-	timezone = location.timezone
+    location = fetch_lat_long(city, i_row)
+    lat      = location.latitude
+    long     = location.longitude
+    timezone = location.timezone
 
-	params_1 = "?latitude=$(lat)&longitude=$(long)&current_weather=true"
+    params_1 = "?latitude=$(lat)&longitude=$(long)&current_weather=true"
     params_2 = "&daily=sunrise,sunset&timezone=$(timezone)"
-	current_dict = get_api_response(params_1 * params_2)
+    current_dict = get_api_response(params_1 * params_2)
 
-	return current_dict
+    return current_dict
 
 end
--- src/request.jl
+++ src/request.jl
@@ -1,9 +1,9 @@
 function get_api_response(params::String, url::String = URL)
 	
 	OM_request = HTTP.request("GET",
-		                       url * params;
-							   verbose = 0,
-							   retries = 2)
+	                           url * params;
+	                           verbose = 0,
+	                           retries = 2)
 	
 	response_text = String(OM_request.body)
 	response_dict = JSON.parse(response_text)
