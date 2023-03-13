538a96d647173134230c6607c5b8f3dc55bf5889
Vikas Negi <vikas.negi10@gmail.com>
2023-03-13 19:01:24 +0100

    Codestyle Blue + version bump

--- src/WeatherReport.jl
+++ src/WeatherReport.jl
@@ -1,21 +1,16 @@
 module WeatherReport
 
-export plot_hourly_temp,
-       plot_hourly_rain,
-       plot_hourly_snow,
-       plot_hourly_humidity,
-       plot_hourly_windspeed,
-       plot_hourly_solar,
-       show_current,
-       show_daily
+export plot_hourly_temp, plot_hourly_rain, plot_hourly_snow
+export plot_hourly_humidity, plot_hourly_windspeed, plot_hourly_solar
+export show_current, show_daily
 
-using HTTP,
-      JSON,
-      CSV,
-      DataFrames,
-      Dates,
-      UnicodePlots,
-      PrettyTables
+using CSV
+using DataFrames
+using Dates
+using HTTP
+using JSON
+using PrettyTables
+using UnicodePlots
 
 include("types.jl")
 include("request.jl")
