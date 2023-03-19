215e76500bda6f9ab0b9b575ce104a5062bca958
Vikas Negi <vikas.negi10@gmail.com>
2023-03-19 18:20:52 +0100

    Add test for hourly dust

--- test/test_plots_air_quality.jl
+++ test/test_plots_air_quality.jl
@@ -7,6 +7,11 @@
         @test sizeof(plt) > 0
     end
 
+    @testset "plot_hourly_dust" begin
+        plt = plot_hourly_dust("Doha", days = 3)
+        @test sizeof(plt) > 0
+    end
+
 end
 
 @testset verbose = true "Plotting with multiple matches" begin
@@ -19,6 +24,14 @@ end
         @test sizeof(plt) > 0
     end
 
+    @testset "plot_hourly_dust" begin
+        plt = plot_hourly_dust("Cairo", 1, days = 3)
+        @test sizeof(plt) > 0
+
+        plt = plot_hourly_dust("Cairo", 3, days = 3)
+        @test sizeof(plt) > 0
+    end
+
 end
 
 @testset verbose = true "Plotting with lat/long input" begin
@@ -26,6 +39,12 @@ end
     @testset "plot_hourly_pm" begin
         # Denver
         plt = plot_hourly_pm(lat = 39.7392, long = -104.985, days = 3) 
+        @test sizeof(plt) > 0
+    end
+
+    @testset "plot_hourly_dust" begin
+        # Delhi
+        plt = plot_hourly_dust(lat = 28.6519, long = 77.2315, days = 4)
         @test sizeof(plt) > 0
     end
 
