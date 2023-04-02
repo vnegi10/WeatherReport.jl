50f8b9ac82c2d49d4a31e16bb08d873b3779ffe3
Vikas Negi <68782261+vnegi10@users.noreply.github.com>
2023-04-02 23:06:52 +0200

    Compare same month (#41)
    * Yearly plots for same month are OK
    
    * Add docstring
    
    * Add timezone + attribution to plot_box_temp
    
    * Add tests
    
    * Bump version + all tests are OK
    
    * Add example to README

--- test/test_plots_history_distribution.jl
+++ test/test_plots_history_distribution.jl
@@ -4,7 +4,14 @@
 
     @testset "plot_box_temp" begin
         plt = plot_box_temp("Veldhoven",
-                             year = "2020")
+                             year = "2023")
+        @test sizeof(plt) > 0
+    end
+
+    @testset "compare_box_temp" begin
+        plt = compare_box_temp("Veldhoven",
+                                num_years = 5,
+                                month = "Jan")
         @test sizeof(plt) > 0
     end
 
@@ -24,6 +31,20 @@ end
         @test sizeof(plt) > 0
     end
 
+    @testset "compare_box_temp" begin
+        plt = compare_box_temp("Madrid",
+                               2,
+                               num_years = 5,
+                               month = "Jan")
+        @test sizeof(plt) > 0
+
+        plt = compare_box_temp("Madrid",
+                               4,
+                               num_years = 5,
+                               month = "Dec")
+        @test sizeof(plt) > 0
+    end
+
 end
 
 @testset verbose = true "Plotting with lat/long input" begin
@@ -33,6 +54,15 @@ end
         plt = plot_box_temp(lat = 39.7392,
                             long = -104.985,
                             year = "2022")
+        @test sizeof(plt) > 0
+    end
+
+    @testset "compare_box_temp" begin
+        # Denver
+        plt = compare_box_temp(lat = 39.7392,
+                               long = -104.985,
+                               num_years = 5,
+                               month = "March")
         @test sizeof(plt) > 0
     end
 
