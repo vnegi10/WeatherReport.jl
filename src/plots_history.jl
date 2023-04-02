096ee0a1da2942fc2427c7679d0ec50054a25326
Vikas Negi <vikas.negi10@gmail.com>
2023-04-02 17:11:36 +0200

    Add docstring

--- src/plots_history.jl
+++ src/plots_history.jl
@@ -130,6 +130,44 @@ function plot_hist_temp(city::String = "",
 end
 
 """
+    plot_box_temp(city::String = "",
+                  i_row::Int64 = 1;
+                  lat::Float64 = 0.0,
+                  long::Float64 = 0.0,
+                  year::String = "2022")
+
+Shows the monthly boxplot distribution of hourly air temperature [°C]
+at 2 meter above ground for a given location (city or lat/long).
+
+# Arguments
+- `city::String` : Valid city name, e.g. "Oslo", "Paris", "Amsterdam" etc.
+- `i_row::Int64` : In case of more than one match for a given location,
+                   select the desired timezone by providing the row index
+                   from the printed DataFrame. Default is set to 1.
+- `year::String` : Year for which data needs to be shown, e.g. "2020"
+
+# Optional keywords
+- `lat::Float64` : Geographical WGS84 coordinate of the location (°S < 0, °N > 0)
+- `long::Float64` : Geographical WGS84 coordinate of the location (°W < 0, °E > 0)
+
+# Example
+```julia-repl
+julia> plot_box_temp("Eindhoven", year = "2023")
+                             Eindhoven: Air temp. distribution for 2023)                  
+            ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+            ┃               ╷           ┌─────────┬─────────┐                ╷          ┃ 
+    January ┃               ├───────────┤         │         ├────────────────┤          ┃ 
+            ┃               ╵           └─────────┴─────────┘                ╵          ┃ 
+            ┃              ╷                 ┌──────┬─────┐        ╷                    ┃ 
+   February ┃              ├─────────────────┤      │     ├────────┤                    ┃ 
+            ┃              ╵                 └──────┴─────┘        ╵                    ┃ 
+            ┃                ╷              ┌─────────┬───────┐              ╷          ┃ 
+      March ┃                ├──────────────┤         │       ├──────────────┤          ┃ 
+            ┃                ╵              └─────────┴───────┘              ╵          ┃ 
+            ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+             -10                                 5                                    20  
+                                                 [°C]                                     
+```
 """
 function plot_box_temp(city::String = "",
                        i_row::Int64 = 1;
