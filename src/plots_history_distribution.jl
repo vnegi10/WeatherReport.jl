2c359edce04cf410b9cf32a3dce82f46b02900a8
Vikas Negi <vikas.negi10@gmail.com>
2023-04-06 00:02:47 +0200

    Add docstring

--- src/plots_history_distribution.jl
+++ src/plots_history_distribution.jl
@@ -182,6 +182,46 @@ function compare_box_temp(city::String = "",
 end
 
 """
+    plot_box_rain(city::String = "",
+                  i_row::Int64 = 1;
+                  lat::Float64 = 0.0,
+                  long::Float64 = 0.0,
+                  year::String = "2022")
+
+Shows the monthly boxplot distribution of rain from large
+scale weather systems of the preceding hour in millimeter [mm]
+for a given location (city or lat/long).
+
+# Arguments
+- `city::String` : Valid city name, e.g. "Oslo", "Paris", "Amsterdam" etc.
+- `i_row::Int64` : In case of more than one match for a given location,
+                   select the desired timezone by providing the row index
+                   from the printed DataFrame. Default is set to 1.
+- `year::String` : Year for which data needs to be shown, e.g. "2020"
+
+# Optional keywords
+- `lat::Float64` : Geographical WGS84 coordinate of the location (°S < 0, °N > 0)
+- `long::Float64` : Geographical WGS84 coordinate of the location (°W < 0, °E > 0)
+
+# Example
+```julia-repl
+julia> plot_box_rain("Mumbai", year = "2023")
+                              Mumbai: Rain monthly distribution for 2023                  
+             Timezone: Asia/Kolkata                     [Weather data by Open-Meteo.com]  
+            ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+            ┃┐      ╷                                                                   ┃ 
+    January ┃├──────┤                                                                   ┃ 
+            ┃┘      ╵                                                                   ┃ 
+            ┃╷                                                                          ┃ 
+   February ┃┤                                                                          ┃ 
+            ┃╵                                                                          ┃ 
+            ┃┐                                                                         ╷┃ 
+      March ┃├─────────────────────────────────────────────────────────────────────────┤┃ 
+            ┃┘                                                                         ╵┃ 
+            ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+             0                                  0.5                                    1  
+                                                 [mm]                                     
+```
 """
 function plot_box_rain(city::String = "",
                        i_row::Int64 = 1;
