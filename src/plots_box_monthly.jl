0ec3724a269c33d8e31257132bc0ec5a3cbc9382
Vikas Negi <vikas.negi10@gmail.com>
2023-04-08 23:39:09 +0200

    Add plot_box_humidity

--- src/plots_box_monthly.jl
+++ src/plots_box_monthly.jl
@@ -218,3 +218,76 @@ function plot_box_snow(city::String = "",
     return plt
 
 end
+
+"""
+    plot_box_humidity(city::String = "",
+                      i_row::Int64 = 1;
+                      lat::Float64 = 0.0,
+                      long::Float64 = 0.0,
+                      year::String = "2022")
+
+Shows the monthly boxplot distribution of relative humidity
+at 2 meter above ground for a given location (city or lat/long).
+
+# Arguments
+- `city::String` : Valid city name, e.g. "Oslo", "Paris", "Amsterdam" etc.
+- `i_row::Int64` : In case of more than one match for a given location,
+                   select the desired timezone by providing the row index
+                   from the printed DataFrame. Default is set to 1.
+- `year::String` : Year for which data needs to be shown, e.g. "2020"
+
+# Optional keywords
+- `lat::Float64` : Geographical WGS84 coordinate of the location (°S < 0, °N > 0)
+- `long::Float64` : Geographical WGS84 coordinate of the location (°W < 0, °E > 0)
+
+# Example
+```julia-repl
+julia> plot_box_humidity("Dubrovnik", year = "2023")
+                          Dubrovnik: Relative humidity distribution for 2023              
+             Timezone: Europe/Zagreb                    [Weather data by Open-Meteo.com]  
+            ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+            ┃            ╷                        ┌────────────┬────────────┐          ╷┃ 
+    January ┃            ├────────────────────────┤            │            ├──────────┤┃ 
+            ┃            ╵                        └────────────┴────────────┘          ╵┃ 
+            ┃       ╷               ┌───────────────────────┬──────────────┐           ╷┃ 
+   February ┃       ├───────────────┤                       │              ├───────────┤┃ 
+            ┃       ╵               └───────────────────────┴──────────────┘           ╵┃ 
+            ┃╷                                      ┌───────────┬────────┐           ╷  ┃ 
+      March ┃├──────────────────────────────────────┤           │        ├───────────┤  ┃ 
+            ┃╵                                      └───────────┴────────┘           ╵  ┃ 
+            ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+             30                                  65                                  100  
+                                                 [%]                                      
+```
+"""
+function plot_box_humidity(city::String = "",
+                           i_row::Int64 = 1;
+                           lat::Float64 = 0.0,
+                           long::Float64 = 0.0,
+                           year::String = "2022")
+
+    all_months, monthly_snow, city, time_zone = get_plotting_data("relativehumidity_2m",
+                                                                  city,
+                                                                  i_row,
+                                                                  lat,
+                                                                  long,
+                                                                  year)
+
+    plt = boxplot(
+        all_months,
+        monthly_snow,
+        title = "$(city): Relative humidity distribution for $(year)",
+        xlabel = "[%]",
+        border = :bold,
+        canvas = BrailleCanvas,
+        width = 75,
+        height = 15,
+        grid = true
+    )
+
+    label!(plt, :tl, "Timezone: $(time_zone)")
+    label!(plt, :tr, ATTRIBUTION)
+
+    return plt
+
+end
