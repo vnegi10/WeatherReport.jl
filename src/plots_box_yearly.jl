5ff8df895a7ad065900b7bc00b34b16e1bcacd3f
Vikas Negi <vikas.negi10@gmail.com>
2023-04-09 23:52:35 +0200

    Add compare_box_rain

--- src/plots_box_yearly.jl
+++ src/plots_box_yearly.jl
@@ -83,3 +83,86 @@ function compare_box_temp(city::String = "",
     return plt
 
 end
+
+"""
+    compare_box_rain(city::String = "",
+                     i_row::Int64 = 1;
+                     lat::Float64 = 0.0,
+                     long::Float64 = 0.0,
+                     month::String = "Jan",
+                     num_years::Int64 = 5)
+
+Compares the boxplot distribution of rain from large
+scale weather systems of the preceding hour in millimeter [mm]
+for a given month across a given number of years.
+
+# Arguments
+- `city::String` : Valid city name, e.g. "Oslo", "Paris", "Amsterdam" etc.
+- `i_row::Int64` : In case of more than one match for a given location,
+                   select the desired timezone by providing the row index
+                   from the printed DataFrame. Default is set to 1.
+
+# Mandatory keywords
+- `month::String` : Month for which data needs to be compared, e.g. "Jan", "March" etc.
+- `num_years::Int64` : Number of years across which the data will be compared
+
+# Optional keywords
+- `lat::Float64` : Geographical WGS84 coordinate of the location (°S < 0, °N > 0)
+- `long::Float64` : Geographical WGS84 coordinate of the location (°W < 0, °E > 0)
+
+# Example
+```julia-repl
+julia> compare_box_rain("Dubrovnik", month = "March", num_years = 3)
+                    Dubrovnik: Rain comparison for March over last 3 years            
+         Timezone: Europe/Zagreb                    [Weather data by Open-Meteo.com]  
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+        ┃┐                                                 ╷                        ┃ 
+   2020 ┃├─────────────────────────────────────────────────┤                        ┃ 
+        ┃┘                                                 ╵                        ┃ 
+        ┃┐                                                   ╷                      ┃ 
+   2021 ┃├───────────────────────────────────────────────────┤                      ┃ 
+        ┃┘                                                   ╵                      ┃ 
+        ┃┐                                                                    ╷     ┃ 
+   2022 ┃├────────────────────────────────────────────────────────────────────┤     ┃ 
+        ┃┘                                                                    ╵     ┃ 
+        ┃┐                                                     ╷                    ┃ 
+   2023 ┃├─────────────────────────────────────────────────────┤                    ┃ 
+        ┃┘                                                     ╵                    ┃ 
+        ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+         0                                   3                                     6  
+                                             [mm]                              
+```
+"""
+function compare_box_rain(city::String = "",
+                          i_row::Int64 = 1;
+                          lat::Float64 = 0.0,
+                          long::Float64 = 0.0,
+                          month::String = "Jan",
+                          num_years::Int64 = 5)
+
+    all_years, yearly_rain, city, time_zone = get_plotting_data("rain",
+                                                                city,
+                                                                i_row,
+                                                                lat,
+                                                                long,
+                                                                month,
+                                                                num_years)
+
+    plt = boxplot(
+        all_years,
+        yearly_rain,
+        title = "$(city): Rain comparison for $(month) over last $(num_years) years",
+        xlabel = "[mm]",
+        border = :bold,
+        canvas = BrailleCanvas,
+        width = 75,
+        height = 15,
+        grid = true
+    )
+
+    label!(plt, :tl, "Timezone: $(time_zone)")
+    label!(plt, :tr, ATTRIBUTION)
+
+    return plt
+
+end
