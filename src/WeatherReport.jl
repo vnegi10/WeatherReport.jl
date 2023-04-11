b767de7c502d3146aedfc6450a19ec2d50d38ddc
Vikas Negi <vikas.negi10@gmail.com>
2023-04-11 22:19:57 +0200

    Add compare_box_humidity

--- src/WeatherReport.jl
+++ src/WeatherReport.jl
@@ -10,6 +10,7 @@ export plot_hist_humidity, plot_hist_windspeed, plot_hist_solar
 export plot_box_temp, plot_box_rain, plot_box_snow
 export plot_box_humidity, plot_box_windspeed, plot_box_solar
 export compare_box_temp, compare_box_rain, compare_box_snow
+export compare_box_humidity
 
 using CSV
 using DataFrames
--- src/plots_box_yearly.jl
+++ src/plots_box_yearly.jl
@@ -249,3 +249,83 @@ function compare_box_snow(city::String = "",
     return plt
 
 end
+
+"""
+    compare_box_humidity(city::String = "",
+                         i_row::Int64 = 1;
+                         lat::Float64 = 0.0,
+                         long::Float64 = 0.0,
+                         month::String = "Jan",
+                         num_years::Int64 = 5)
+
+Compares the boxplot distribution of relative humidity
+at 2 meter above ground for a given location (city or lat/long)
+for a given month across a given number of years.
+
+# Arguments
+- `city::String` : Valid city name, e.g. "Oslo", "Paris", "Amsterdam" etc.
+- `i_row::Int64` : In case of more than one match for a given location,
+                   select the desired timezone by providing the row index
+                   from the printed DataFrame. Default is set to 1.
+
+# Mandatory keywords
+- `month::String` : Month for which data needs to be compared, e.g. "Jan", "March" etc.
+- `num_years::Int64` : Number of years across which the data will be compared
+
+# Optional keywords
+- `lat::Float64` : Geographical WGS84 coordinate of the location (°S < 0, °N > 0)
+- `long::Float64` : Geographical WGS84 coordinate of the location (°W < 0, °E > 0)
+
+# Example
+```julia-repl
+julia> compare_box_humidity("Dubrovnik", month = "Feb", num_years = 2)
+              Dubrovnik: Relative humidity comparison for Feb over last 2 years       
+         Timezone: Europe/Zagreb                    [Weather data by Open-Meteo.com]  
+        ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ 
+        ┃      ╷                              ┌────────────────┬──────────┐        ╷┃ 
+   2021 ┃      ├──────────────────────────────┤                │          ├────────┤┃ 
+        ┃      ╵                              └────────────────┴──────────┘        ╵┃ 
+        ┃              ╷                 ┌───────────────┬──────────────┐          ╷┃ 
+   2022 ┃              ├─────────────────┤               │              ├──────────┤┃ 
+        ┃              ╵                 └───────────────┴──────────────┘          ╵┃ 
+        ┃               ╷             ┌─────────────────────┬────────────┐         ╷┃ 
+   2023 ┃               ├─────────────┤                     │            ├─────────┤┃ 
+        ┃               ╵             └─────────────────────┴────────────┘         ╵┃ 
+        ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛ 
+         20                                  60                                  100  
+                                             [%]                   
+```
+"""
+function compare_box_humidity(city::String = "",
+                              i_row::Int64 = 1;
+                              lat::Float64 = 0.0,
+                              long::Float64 = 0.0,
+                              month::String = "Jan",
+                              num_years::Int64 = 5)
+
+    all_years, yearly_hum, city, time_zone = get_plotting_data("relativehumidity_2m",
+                                                                city,
+                                                                i_row,
+                                                                lat,
+                                                                long,
+                                                                month,
+                                                                num_years)
+
+    plt = boxplot(
+        all_years,
+        yearly_hum,
+        title = "$(city): Relative humidity comparison for $(month) over last $(num_years) years",
+        xlabel = "[%]",
+        border = :bold,
+        canvas = BrailleCanvas,
+        width = 75,
+        height = 15,
+        grid = true
+    )
+
+    label!(plt, :tl, "Timezone: $(time_zone)")
+    label!(plt, :tr, ATTRIBUTION)
+
+    return plt
+
+end
