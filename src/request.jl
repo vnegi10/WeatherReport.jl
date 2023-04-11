3fb7cda9fb11b23df1fc22185250d754995b541f
Vikas Negi <68782261+vnegi10@users.noreply.github.com>
2023-04-11 20:33:22 +0200

    Add basic check (#54)
    * Add basic check
    
    * Version bump

--- src/request.jl
+++ src/request.jl
@@ -1,9 +1,18 @@
 function get_api_response(params::String, url::String)
-	
-    OM_request = HTTP.request("GET",
-                               url * params;
-                               verbose = 0,
-                               retries = 2)
+
+    OM_request = nothing
+    try
+        OM_request = HTTP.request("GET",
+                                  url * params;
+                                  verbose = 0,
+                                  retries = 2)
+    catch e
+        if isa(e, HTTP.ExceptionRequest.StatusError)
+            error("Check if the input is valid")
+        else
+            error("Could not fetch data, try again later!")
+        end
+    end
 	
     response_text = String(OM_request.body)
     response_dict = JSON.parse(response_text)
