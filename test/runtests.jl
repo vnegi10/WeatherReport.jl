5ced6427a56c5ca03878f5a5b86b2543e3183440
Vikas Negi <vikas.negi10@gmail.com>
2023-05-13 21:14:42 +0200

    Test selection works OK

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,54 +1,33 @@
 using ReTestItems, WeatherReport
 
-@time runtests(WeatherReport)
+num_threads = Threads.nthreads()
+num_workers = 1
 
-#=errors = false
+if num_threads == 1
+    @info "Using only 1 thread, try starting the Julia session with more threads!"
+else
+    # Each worker uses two threads by default
+    num_workers = floor(Int, num_threads/2)
+end
+
 all_tests = false
+errors = false
 
 # Run all tests when no arguments or "all" is specified
 if isempty(ARGS) || "all" in ARGS
     all_tests = true
 end
 
-# Execute test in a try-catch block for each file
-function execute_test(file::String, ARGS; all_tests::Bool)
-
-    if all_tests || file in ARGS
-        try
-            include(file)
-            println("\t\033[1m\033[32mPASSED\033[0m: $(file)")
-        catch e
-            println("\t\033[1m\033[31mFAILED\033[0m: $(file)")
-            global errors = true
-            showerror(stdout, e, backtrace())
-            println()
-        end
+if all_tests
+    @time WeatherReport.execute_test(runtests(WeatherReport, nworkers = num_workers))
+else
+    @time for find_match in ARGS
+        WeatherReport.execute_test(runtests(WeatherReport;
+                                            name = Regex("$(find_match)"),
+                                            nworkers = 1))
     end
-
-end
-
-################# All test groups #################
-
-test_files = ["test_hourly.jl",
-              "test_plots_forecast.jl",
-              "test_plots_air_quality.jl",
-              "test_plots_history.jl",
-              "test_plots_box_monthly.jl",
-              "test_plots_box_yearly.jl",
-              "test_daily.jl",
-              "test_helpers.jl",
-              "test_exceptions.jl"]
-
-###################################################
-
-################# Execute tests ###################
-
-@time for file in test_files
-    execute_test(file, ARGS, all_tests = all_tests)
 end
 
 if errors
     throw("Some tests have failed! Check the results summary above.")
-end=#
-
-###################################################
+end
